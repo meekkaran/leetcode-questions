4d5338da6fc6ca90dc58558e5c8ba447ffb315f4
Meek Karan <60463223+meekkaran@users.noreply.github.com>
2021-12-03 09:16:51 +0300

    Update MissingNumber.py

--- Bit Manipulation/MissingNumber.py
+++ Bit Manipulation/MissingNumber.py
@@ -1,3 +1,4 @@
+# Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         nums.sort()
@@ -5,3 +6,11 @@ class Solution:
         for i in range(len(nums)):
             result +=(i - nums[i])
         return result
+        
+        
+        # solution2
+        N = len(nums)
+        ExpSum = N*(N+1)//2
+        ActSum = sum(nums)
+        
+        return ExpSum- ActSum
