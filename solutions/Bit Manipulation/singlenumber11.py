a2f734c68e24d916ea3805f6ba2ed5fce3c7ce36
Meek Karan <60463223+meekkaran@users.noreply.github.com>
2022-04-10 08:49:38 +0300

    Update singlenumber11.py
    solve it just as singlenumber1

--- solutions/Bit Manipulation/singlenumber11.py
+++ solutions/Bit Manipulation/singlenumber11.py
@@ -1,3 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         return (3 * sum(set(nums)) - sum(nums))//2
+    
+#     solution2
+#             in the second solution , you could solve it just the same way as solving the question on single number
+        c= Counter(nums)
+        for i in c:
+            if c[i] == 1:
+                return i
+        
