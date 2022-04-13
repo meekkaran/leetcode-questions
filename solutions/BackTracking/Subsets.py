cdedc1ceaccc6dc33c829564d4f364e58cbda2ae
Meek Karan <60463223+meekkaran@users.noreply.github.com>
2022-04-13 10:08:49 +0300

    Update Subsets.py

--- solutions/BackTracking/Subsets.py
+++ solutions/BackTracking/Subsets.py
@@ -1,8 +1,12 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         #solve it iteratively
+        #use cascading method, 
+        #output = [[]]
+        #step1: add each number in nums inside the array which is inside the output array
+        #[[],[1], [2], [1,2], [3], [1,3], [2,3], [1,2,3]]
         output = [[]]
-        for n in nums:
-            output += [ o + [n] for o in output]
+        for i in nums:
+            output += [o + [i] for o in output]
         return output
             
