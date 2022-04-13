a884e0de9fdd9be260361b8ede61a525babe928a
Meek Karan <60463223+meekkaran@users.noreply.github.com>
2022-04-13 10:32:26 +0300

    Update Subsets2.py

--- solutions/BackTracking/Subsets2.py
+++ solutions/BackTracking/Subsets2.py
@@ -1,13 +1,14 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
+         nums.sort()  # sort the numbers before converting the list to a tuple
         ans = [[]]
         for n in nums:
             temp=[]
             for l in ans:
                 temp.append(l+[n])
             ans.extend(temp)
-        #1.convert the list into  a tuple
-        #2.make it a set
-        #3.then reconvert it into a list
+           
+        #1.convert the list into  a tuple       [tuple(l) for l in ans]
+        #2.make it a set                        set([tuple(l) for l in ans])
+        #3.then reconvert it into a list        list[l] for l in set([tuple(l) for l in ans])
         return [list(l) for l in set([tuple (l) for l in ans])]
