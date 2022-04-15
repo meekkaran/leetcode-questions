69c12c1947eb4b6a416781d125263b26b98b6335
Meek Karan <60463223+meekkaran@users.noreply.github.com>
2022-04-15 12:03:05 +0300

    Delete BestTimeToBuyAnsSellStock.py

--- solutions/BestTimeToBuyAnsSellStock.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        maxProfit  = 0
-        minPrice = 10000
-        for price in prices:
-            minPrice = min(minPrice, price)
-            maxProfit = max(maxProfit, price-minPrice)
-        return maxProfit
-        
