e0a9916790d34e1a4d676fe49ce8b789aea9a9a2
Meek Karan <60463223+meekkaran@users.noreply.github.com>
2022-04-29 14:54:07 +0300

    Update SpiralMatrix.py

--- solutions/Arrays/SpiralMatrix.py
+++ solutions/Arrays/SpiralMatrix.py
@@ -1,29 +1,39 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix: return [] #return an empty list if there is no matrix
-        output = []
-        #initialise rows and columns
-        start_row = 0
-        end_row = len(matrix)
-        start_column = 0
-        end_column = len(matrix[0])
+        res = []
+        #have 4 pointers on diffent positions in the matrix
+        left = 0;
+        top = 0;
+        right=len(matrix[0])
+        bottom=len(matrix)#no. of rows
+        #we are moving to the right, then down, then to the left, and finally moving up.
+        #remember to update pointers when moving from one direction to another
         
-        while start_row < end_row or start_column < end_column:
-            #right
-            if start_row < end_row:
-                output.extend([matrix[start_row][i] for i in range(start_column,end_column)])
-                start_row +=1
-            #down
-            if start_column < end_column:
-                output.extend([matrix[i][end_column-1] for i in range(start_row,end_row)])
-                end_column -=1
-            #left
-            if start_row < end_row:
-                output.extend([matrix[end_row-1][i] for i in range(end_column -1,start_column -1, -1)])
-                end_row -=1
-            #up
-            if start_column < end_column:
-                output.extend([matrix[i][start_column] for i in range(end_row -1,start_row -1, -1)])
-                start_column +=1 
+        while left < right and top < bottom:
+            #get every i in the top row
+            for i in range(left,right):
+                res.append(matrix[top][i])
+            top += 1#shift top pointer down by 1
+            
+            #get every i in the right column
+            for i in range(top,bottom):
+                res.append(matrix[i][right-1])
+            right -=1#shift the right pointer to the left
+            
+            if not (left < right and top < bottom):
+                break
+            
+            #get every i in the bottom row
+            for i in range (right-1,left-1,-1):#we are adding -1 since we are doing this in reverse order(right to left)
+                res.append(matrix[bottom-1][i])     
+            bottom -= 1
+            
+            #get every i in the left collumn
+            for i in range(bottom-1,top-1,-1):#we are adding -1 since we are doing this in reverse order(bottom to top)
+                res.append(matrix[i][left])
+            left += 1#shift left pointer to the right
+        return res
+            
                 
-        return output
+        
+        
